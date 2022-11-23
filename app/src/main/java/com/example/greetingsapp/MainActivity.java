ca3833a4d468bf87e92062e73dd649011be2b633
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-23 14:16:27 -0400

    app finalizada

--- app/src/main/java/com/example/greetingsapp/MainActivity.java
+++ app/src/main/java/com/example/greetingsapp/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.greetingsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Step 2: Declaring Widgets
+    TextView textView;
+    Button btn;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        btn = findViewById(R.id.btn);
+        editText = findViewById(R.id.editText1);
+
+        // Step 3: Adding the Functionality
+
+        // tap of the button
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // save user input
+                String name = editText.getText().toString();
+
+                // Displaying the name
+                Toast.makeText(MainActivity.this, "Hello " + name, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
